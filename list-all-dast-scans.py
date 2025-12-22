17595443364e5730824760a6949efe4a6ce36e35
Ricardo Pereira - Veracode <20283870+cadonuno@users.noreply.github.com>
2025-12-22 11:50:36 +0000

    Adding URL and ISM info to output and adding cache to avoid duplicate API calls

--- list-all-dast-scans.py
+++ list-all-dast-scans.py
@@ -3,6 +3,9 @@ import csv
 from veracode_api_py import Analyses, BusinessUnits, Occurrences
 from veracode_api_py.apihelper import APIHelper
 
+ANALYSIS_CACHE = dict()
+URLS_ISMS_CACHE = dict()
+
 def get_actual_start_date(occurrence):
     return occurrence["actual_start_date"] if "actual_start_date" in occurrence else "NONE"
 
@@ -22,6 +25,28 @@ def get_status(occurrence):
     if "status" in occurrence and "status_type" in occurrence["status"]:
         return occurrence["status"]["status_type"]
     return 'No status found'
+
+def parse_ism_info(ism_config):
+    gateway_id = ism_config["gateway_id"]
+    endpoint_id = ism_config["endpoint_id"]
+    return f"ISM Gateway ID: {gateway_id}, Endpoint ID: {endpoint_id}"
+
+def get_urls_isms_for_id(analysis_id):
+    if analysis_id in URLS_ISMS_CACHE:
+        return URLS_ISMS_CACHE.get(analysis_id)
+    
+    #scans are the URLs for each analysis
+    scans = Analyses().get_scans(analysis_id)
+    urls_isms = ", ".join([scan["target_url"] + (" / " + parse_ism_info(scan["internal_scan_configuration"]) if scan["internal_scan_configuration"]["enabled"] else "") for scan in scans])
+    URLS_ISMS_CACHE.update({analysis_id: urls_isms})
+    return urls_isms
+
+def get_analysis_for_id(analysis_id):
+    if analysis_id in ANALYSIS_CACHE:
+        return ANALYSIS_CACHE.get(analysis_id)
+    analyses = Analyses().get(analysis_id)
+    ANALYSIS_CACHE.update({analysis_id: analyses})
+    return analyses
 
 def main():
     parser = argparse.ArgumentParser(
@@ -72,16 +97,19 @@ def main():
         business_unit_map.update({str(bu["bu_legacy_id"]): bu["bu_name"]})
 
     for occurrence in all_occurrences:
-        base_analysis = Analyses().get(occurrence["analysis_id"])
+        base_analysis = get_analysis_for_id(occurrence["analysis_id"])
+        
+        urls_isms = get_urls_isms_for_id(occurrence["analysis_id"])        
+
         bu_id = base_analysis["org_info"]["business_unit_id"] if ("org_info" in base_analysis and "business_unit_id" in base_analysis["org_info"]) else None
         business_unit = business_unit_map[bu_id] if str(bu_id) in business_unit_map else "No Business Unit"
-        all_dast_scans.append({ "name": base_analysis["name"], "business_unit": business_unit, "create_date": get_create_date(base_analysis), "scheduled_start_date": get_scheduled_start_date(occurrence), "actual_start_date": get_actual_start_date(occurrence), "scheduled_end_date": get_scheduled_end_date(occurrence), "actual_end_date": get_actual_end_date(occurrence), "status": get_status(occurrence)})         
+        all_dast_scans.append({ "name": base_analysis["name"], "url_ism": urls_isms, "business_unit": business_unit, "create_date": get_create_date(base_analysis), "scheduled_start_date": get_scheduled_start_date(occurrence), "actual_start_date": get_actual_start_date(occurrence), "scheduled_end_date": get_scheduled_end_date(occurrence), "actual_end_date": get_actual_end_date(occurrence), "status": get_status(occurrence)})         
 
     with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["Name", "Business Unit", "Create Date", "Start Date (scheduled)", "Start Date (actual)", "End Date (scheduled)", "End Date (actual)", "Status"])
+        csv_writer.writerow(["Name", "URL/ISMs", "Business Unit", "Create Date", "Start Date (scheduled)", "Start Date (actual)", "End Date (scheduled)", "End Date (actual)", "Status"])
         for entry in all_dast_scans:
-            csv_writer.writerow([entry["name"], entry["business_unit"], entry["create_date"], entry["scheduled_start_date"], entry["actual_start_date"], entry["scheduled_end_date"], entry["actual_end_date"], entry["status"]])
+            csv_writer.writerow([entry["name"], entry["url_ism"], entry["business_unit"], entry["create_date"], entry["scheduled_start_date"], entry["actual_start_date"], entry["scheduled_end_date"], entry["actual_end_date"], entry["status"]])
 
 if __name__ == '__main__':
     main()
