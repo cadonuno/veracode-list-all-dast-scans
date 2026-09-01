6ee76bac3acb15da64af4a1ea6edfbab3af4f301
Ricardo Alvares Pereira Junior <20283870+cadonuno@users.noreply.github.com>
2026-09-01 18:16:04 +0100

    - Adding support for saving Gateway/Endpoint names - Changing script to save results as Excel file instead of CSV -  Moving individual URLs to separate tab - Adding app linking info

--- README.md
+++ README.md
@@ -22,6 +22,14 @@ Install dependencies:
     veracode_api_key_secret = <YOUR_API_KEY_SECRET>
 
 
+## ISM information
+By default, the script will return the endpoint and gateway IDs. However, it supports the inclusion of an endpoints.csv and a gateways.csv files, which will map these values. An example file can be found for each (endpoints_example.csv and gateways_example.csv).
+
+To extrac the IDs, you can follow the instructions included here: https://docs.veracode.com/r/t_dynamic_ISM. The URL depends on your platform instance, but will be one of these:
+- https://ui.analysiscenter.veracode.com/mvsa/admin/gateways?depth=1
+- https://ui.analysiscenter.veracode.eu/mvsa/admin/gateways?depth=1
+- https://ui.analysiscenter.veracode.us/mvsa/admin/gateways?depth=1
+
 ## Run
 If you have saved credentials as above you can run:
 
@@ -34,6 +42,6 @@ Otherwise you will need to set environment variables:
     python list-all-dast-scans.py (arguments)
 
 ## Supported Arguments:
-- `-o`, `--output_file` - Name of the CSV file to save (default: 'All_DAST_Scans.csv').
+- `-o`, `--output_file` - Name of the XLSX file to save (default: 'All_DAST_Scans.xlsx').
 - `-s`, `--start_date` - Minimum scheduled start date (YYYY-MM-DDTHH:mm:ssZ).
 - `-e`, `--end_date` - Maximum scheduled start date (YYYY-MM-DDTHH:mm:ssZ).
--- list-all-dast-scans.py
+++ list-all-dast-scans.py
@@ -1,11 +1,20 @@
 import argparse
 import csv
-from veracode_api_py import Analyses, BusinessUnits, Occurrences 
+import os
+import pandas as pd
+from veracode_api_py import Analyses, BusinessUnits, Occurrences, Scans
 from veracode_api_py.apihelper import APIHelper
 
 ANALYSIS_CACHE = dict()
 URLS_ISMS_CACHE = dict()
 ENDPOINT_CACHE = dict()
+SCAN_TYPES= {
+    "WEB_SCAN": "Web Application",
+    "API_SCAN": "API",
+}
+
+gateway_id_to_gateway_name = dict()
+endpoint_id_to_endpoint_name = dict()
 
 def get_actual_start_date(occurrence):
     return occurrence["actual_start_date"] if "actual_start_date" in occurrence else "NONE"
@@ -28,6 +37,9 @@ def get_status(occurrence):
     return 'No status found'
 
 def parse_ism_info(ism_info):
+    global gateway_id_to_gateway_name
+    global endpoint_id_to_endpoint_name
+    
     gateway_id = ism_info["gateway_id"]
     endpoint_id = ism_info["endpoint_id"]
     if endpoint_id in ENDPOINT_CACHE:
@@ -44,20 +56,10 @@ def parse_ism_info(ism_info):
                     return endpoint_info
             break
 
-    endpoint_info = f"ISM Gateway ID: {gateway_id}, Endpoint ID: {endpoint_id}"
+    endpoint_info = f"ISM Gateway: {gateway_id_to_gateway_name.get(gateway_id, f' ID {gateway_id}')}, Endpoint: {endpoint_id_to_endpoint_name.get(endpoint_id, f' ID {endpoint_id}')}"
     ENDPOINT_CACHE.update({endpoint_id: endpoint_info})
 
     return endpoint_info
-
-def get_urls_isms_for_id(analysis_id):
-    if analysis_id in URLS_ISMS_CACHE:
-        return URLS_ISMS_CACHE.get(analysis_id)
-    
-    #scans are the URLs for each analysis
-    scans = Analyses().get_scans(analysis_id)
-    urls_isms = ", ".join([scan["target_url"] + (" / " + parse_ism_info(scan["internal_scan_configuration"]) if scan["internal_scan_configuration"]["enabled"] else "") for scan in scans])
-    URLS_ISMS_CACHE.update({analysis_id: urls_isms})
-    return urls_isms
 
 def get_analysis_for_id(analysis_id):
     if analysis_id in ANALYSIS_CACHE:
@@ -65,6 +67,32 @@ def get_analysis_for_id(analysis_id):
     analyses = Analyses().get(analysis_id)
     ANALYSIS_CACHE.update({analysis_id: analyses})
     return analyses
+
+def parse_endpoints_and_gateways_map():
+    global gateway_id_to_gateway_name
+    global endpoint_id_to_endpoint_name
+
+    gateway_csv = 'gateways.csv'
+    endpoint_csv = 'endpoints.csv'
+
+    try:
+        if os.path.exists(gateway_csv):
+            with open(gateway_csv, 'r') as gateways_file:
+                reader = csv.DictReader(gateways_file)
+                for row in reader:
+                    gateway_id_to_gateway_name[row["gateway_id"]] = row["gateway_name"]
+        else:
+            print(f"Warning: {gateway_csv} not found. Gateway names will not be available.")
+
+        if os.path.exists(endpoint_csv):
+            with open(endpoint_csv, 'r') as endpoints_file:
+                reader = csv.DictReader(endpoints_file)
+                for row in reader:
+                    endpoint_id_to_endpoint_name[row["endpoint_id"]] = row["endpoint_name"]
+        else:
+            print(f"Warning: {endpoint_csv} not found. Endpoint names will not be available.")
+    except Exception as e:
+        print(f"Error reading CSV files: {e}")
 
 def main():
     parser = argparse.ArgumentParser(
@@ -96,10 +124,11 @@ def main():
     output_file = args.output_file
 
     if not output_file:
-        output_file = "All_DAST_Scans.csv"
+        output_file = "All_DAST_Scans.xlsx"
+
+    parse_endpoints_and_gateways_map()
 
     print("Fetching list of DAST scans")
-    all_dast_scans = []
     if args.start_date or args.end_date:
         scan_filters = {}
         if args.start_date:
@@ -114,20 +143,43 @@ def main():
     for bu in BusinessUnits().get_all():
         business_unit_map.update({str(bu["bu_legacy_id"]): bu["bu_name"]})
 
+    all_dast_scans = []
+    raw_data = []
     for occurrence in all_occurrences:
         base_analysis = get_analysis_for_id(occurrence["analysis_id"])
         
-        urls_isms = get_urls_isms_for_id(occurrence["analysis_id"])        
-
+        is_api_scan = base_analysis["scan_type"] == "API_SCAN"
         bu_id = base_analysis["org_info"]["business_unit_id"] if ("org_info" in base_analysis and "business_unit_id" in base_analysis["org_info"]) else None
-        business_unit = business_unit_map[bu_id] if str(bu_id) in business_unit_map else "No Business Unit"
-        all_dast_scans.append({ "name": base_analysis["name"], "url_ism": urls_isms, "business_unit": business_unit, "create_date": get_create_date(base_analysis), "scheduled_start_date": get_scheduled_start_date(occurrence), "actual_start_date": get_actual_start_date(occurrence), "scheduled_end_date": get_scheduled_end_date(occurrence), "actual_end_date": get_actual_end_date(occurrence), "status": get_status(occurrence)})         
+        business_unit = business_unit_map[bu_id] if str(bu_id) in business_unit_map else "N/A"
 
-    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["Name", "URL/ISMs", "Business Unit", "Create Date", "Start Date (scheduled)", "Start Date (actual)", "End Date (scheduled)", "End Date (actual)", "Status"])
-        for entry in all_dast_scans:
-            csv_writer.writerow([entry["name"], entry["url_ism"], entry["business_unit"], entry["create_date"], entry["scheduled_start_date"], entry["actual_start_date"], entry["scheduled_end_date"], entry["actual_end_date"], entry["status"]])
+        urls_for_ocurrence = Occurrences().get_scan_occurrences(occurrence["analysis_occurrence_id"])
+        all_dast_scans.append({ "Analysis Name": base_analysis["name"], "Scan Type": SCAN_TYPES.get(base_analysis["scan_type"], "NONE"), "Number of Items": len(urls_for_ocurrence), "Status": get_status(occurrence), "Frequency": base_analysis.get("schedule_frequency", "NONE"), "StartDate": get_scheduled_start_date(occurrence), "Business Unit": business_unit })
+        
+        for url_scan_ocurrence in urls_for_ocurrence:
+            very_high_flaws = url_scan_ocurrence.get("count_of_very_high_sev_flaws", 0)
+            high_flaws = url_scan_ocurrence.get("count_of_high_sev_flaws", 0)
+            medium_flaws = url_scan_ocurrence.get("count_of_medium_sev_flaws", 0)
+            low_flaws = url_scan_ocurrence.get("count_of_low_sev_flaws", 0)
+            total_flaws = very_high_flaws + high_flaws + medium_flaws + low_flaws
+            raw_data.append({
+                                "Analysis Name": base_analysis["name"], 
+                                "URL/Api SPEC": url_scan_ocurrence["api_scan_setting"]["spec_name"] if is_api_scan else url_scan_ocurrence["target_url"], 
+                                "Application Name": url_scan_ocurrence.get("linked_platform_app_name", "N/A"), 
+                                "Status": url_scan_ocurrence.get("analysis_occurrence_status", "N/A"), 
+                                "Very High": very_high_flaws, 
+                                "High": high_flaws, 
+                                "Medium": medium_flaws, 
+                                "Low": low_flaws, 
+                                "Total Count": total_flaws, 
+                                "Start Date": url_scan_ocurrence.get("start_date", "N/A"), 
+                                "Duration": url_scan_ocurrence.get("duration", "N/A"), 
+                                "Internal Scanning": parse_ism_info(url_scan_ocurrence["internal_scan_configuration"]) if url_scan_ocurrence["internal_scan_configuration"]["enabled"] else "N/A"
+                            })
 
+    with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
+        df = pd.DataFrame(all_dast_scans)
+        df.to_excel(writer, sheet_name='All Analysis Completed', index=False)
+        df_raw = pd.DataFrame(raw_data)
+        df_raw.to_excel(writer, sheet_name='Raw Data', index=False)
 if __name__ == '__main__':
     main()
